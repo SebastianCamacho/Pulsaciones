23389eb8e7d07cd8d94f059e3f351e94397f9be2
Sebastian Camacho <jscamacho@unicesar.edu.co>
2020-09-24 23:26:23 -0500

    Si guarda y muestra

---
+++ pulsacionesdotnet/ClientApp/src/app/pulsacion/persona-registro/persona-registro.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Persona } from '../models/persona';
+import {PersonaLService} from '../../services/persona.service';
+
+@Component({
+  selector: 'app-persona-registro',
+  templateUrl: './persona-registro.component.html',
+  styleUrls: ['./persona-registro.component.css']
+})
+export class PersonaRegistroComponent implements OnInit {
+  persona: Persona;
+
+  constructor(private personaService: PersonaLService) { }
+
+  ngOnInit(): void {
+    this.persona = new Persona;
+  }
+  add() {
+    alert('Se Agrego una nueva persona' + JSON.stringify(this.persona));
+    this.personaService.post(this.persona);
+  }
+
+}
